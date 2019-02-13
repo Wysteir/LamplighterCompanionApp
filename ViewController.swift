fb948fd03bd4921492f6dd2f52b4cd731b530071
Wysteir <wysteir@gmail.com>
2019-02-13 15:36:05 -0500

    Created Project
    Copied a basic project into our repository

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LamplighterCompanionApp
+//
+//  Created by Castro Eugene Allen A. on 2/13/19.
+//  Copyright © 2019 Castro Eugene Allen A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
