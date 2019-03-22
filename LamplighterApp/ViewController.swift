926d7d57b567c5b87ed4e9767c0853c705c4c084
Castro Eugene Allen A <>
2019-03-22 15:17:44 -0400

    Added in base Tap recognizer, included values for press and hold for swiping for both time held and time between inputs

--- LamplighterApp/ViewController.swift
+++ LamplighterApp/ViewController.swift
@@ -11,12 +11,41 @@ import UIKit
 class ViewController: UIViewController {
 
     var holdingCounter:Int = 0;
+    var releaseCounter:Int = 0;
+    let releaseCountMax:Int = 100;
     var isHolding:Bool = false;
     private var lastSwipeBeginningPoint: CGPoint?
+    
+    //let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+    
+    @objc func fireTimer() {
+        //print("Timer fired!")
+        if(isHolding){
+            if(releaseCounter != 0){
+                //print("Release")
+                //print(releaseCounter)
+                releaseCounter=0
+            }//end if release counter
+            holdingCounter+=1;
+        }//end if isholding
+        else{
+            if(holdingCounter != 0){
+                //print("Holding")
+                //print(holdingCounter)
+                holdingCounter = 0
+            }//end if holding counter
+            if(releaseCounter<releaseCountMax){
+                releaseCounter+=1
+            }//end if releaseCounter
+        }//end else isHolding
+    }//end firetimer
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let timer = Timer(timeInterval: 0.1, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer, forMode: .commonModes)
         
         /*let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture(gesture:)))
         swipeLeft.direction = .left
@@ -34,6 +63,9 @@ class ViewController: UIViewController {
         swipeDown.direction = .down
         self.view.addGestureRecognizer(swipeDown)*/
         
+        let tapRec = UITapGestureRecognizer(target: self, action: #selector(handleTap(gesture:)))
+        self.view.addGestureRecognizer(tapRec)
+        
         let panRec = UIPanGestureRecognizer(target: self, action: #selector(handleSwipe(gesture:)))
         self.view.addGestureRecognizer(panRec)
         
@@ -44,6 +76,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func handleTap(gesture: UITapGestureRecognizer) -> Void {
+        if gesture.state == .ended {
+            // handling code
+            print("Tap")
+        }//end if
+    }//end handle tap
+    
     
     @objc func handleSwipe(gesture: UISwipeGestureRecognizer) -> Void {
         /*print(gesture.direction.rawValue)
@@ -62,19 +102,20 @@ class ViewController: UIViewController {
         }//end switch*/
         
         if gesture.state == .began {
-            //print("Begin")
-            
+            print("Begin")
+            isHolding = true;
             lastSwipeBeginningPoint = gesture.location(in: gesture.view)
         }//end if
         else if gesture.state == .ended {
-            //print("End")
+            print("End")
             guard let beginPoint = lastSwipeBeginningPoint else {
                 return
             }
+            isHolding = false;
             let endPoint = gesture.location(in: gesture.view)
-            print("Point: ")
-            print(endPoint.x - beginPoint.x)
-            print(endPoint.y - beginPoint.y)
+            //print("Point: ")
+            //print(endPoint.x - beginPoint.x)
+            //print(endPoint.y - beginPoint.y)
         }//end else if
         
     }//end handle gesture
